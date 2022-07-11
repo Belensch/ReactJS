0b538a2cea1c1e269ec8c10f072e7a9974060f12
Belen Schmid <belensch@hotmail.com.ar>
2022-07-11 17:41:42 -0300

    navBar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+
+
+
+function App() {
+    return ( 
+        <div className = "App" >
+        <NavBar />
+        <div className = 'ejemplo' >
+        <p > Belen Schmid </p>     </div> <ItemListContainer titulo = "Bienvenido a La Nueva Vera Pizza"
+        subtitulo = "El delivery que buscabas." / >
+        </div>
+    )
+}
+
+export default App
